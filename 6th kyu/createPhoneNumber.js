645afba19cb77a24b09fbdcba179c8967c147863
Jacob Asher <pimpdogbark@gmail.com>
2022-07-15 18:58:43 -0400

    Create createPhoneNumber.js

---
+++ 6th kyu/createPhoneNumber.js
@@ -0,0 +1,10 @@
+function createPhoneNumber(numbers){
+  let num1 = "("+numbers[0]+numbers[1]+numbers[2]
+  let num2 = ") "+numbers[3]+numbers[4]+numbers[5]+"-"+numbers[6]+numbers[7]+numbers[8]+numbers[9]
+  
+  return num1+num2
+  
+}
+
+// try for loop .replace()
+// try .join() and .substring()
