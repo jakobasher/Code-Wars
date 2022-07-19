70be380cc911c04d3e3bb3ddc83d4ddf7c562335
Jacob Asher <pimpdogbark@gmail.com>
2022-07-19 16:25:13 -0400

    Create numberOfTrailingZerosOfN.js

---
+++ 5th kyu/numberOfTrailingZerosOfN.js
@@ -0,0 +1,14 @@
+function zeros (n) {
+  let count = 0
+
+  for (let i = 5; Math.floor(n/i) >= 1; i *= 5) {
+      count += Math.floor(n/i)
+  }
+  
+  return count
+
+}
+
+
+// Legendre's Formula ?
+// n / (5^i)
