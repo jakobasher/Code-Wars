32d6ba18a97088b52af26bf80c48d4a4e22f29a7
Jacob Asher <pimpdogbark@gmail.com>
2022-08-17 17:57:37 -0400

    Create makeAcronym.js

---
+++ 7th kyu/makeAcronym.js
@@ -0,0 +1,11 @@
+function toAcronym(inp){
+  
+  return inp.split(" ").map(e=>e[0].toUpperCase()).join("")
+
+
+}
+
+// .split() string to array
+// .map() function each element
+// .toUpperCase() element to uppercase
+// .join() array to string
