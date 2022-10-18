e81eaee7fcf3edd55d49a9e6ffb5565ff5a2289b
Jakob Asher <pimpdogbark@gmail.com>
2022-10-17 20:12:47 -0400

    Create teaForTwo.js

---
+++ 7th kyu/teaForTwo.js
@@ -0,0 +1,7 @@
+function tea42(input) {
+  return input.toString().replace (/2/g, 't')
+};
+
+// .toString() returns string
+// .replace() replaces first instance of string with replacement
+// "/ /g" REGEX replaces all instances of
