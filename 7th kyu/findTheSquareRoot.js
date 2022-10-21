fd607485a20c684cdf18c6bf2f5db94c775de3cd
Jakob Asher <pimpdogbark@gmail.com>
2022-10-20 21:55:38 -0400

    Create findTheSquareRoot.js

---
+++ 7th kyu/findTheSquareRoot.js
@@ -0,0 +1,8 @@
+function squareRoot(x) {
+  return +(x**0.5).toFixed(5)
+}
+
+// Alternative to Math.pow(), Math.sqrt()
+// +var returns integer
+// ** Exponentiation similar to Math.pow()
+// .toFixed() formats number to fixed point notation
