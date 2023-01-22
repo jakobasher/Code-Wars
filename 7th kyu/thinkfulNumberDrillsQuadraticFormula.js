9b93660eb5f3431b10d4f657f71c7ddbfb17abc6
Jakob Asher <pimpdogbark@gmail.com>
2023-01-22 15:30:35 -0500

    Create thinkfulNumberDrillsQuadraticFormula.js

---
+++ 7th kyu/thinkfulNumberDrillsQuadraticFormula.js
@@ -0,0 +1,6 @@
+function quadraticFormula(a, b, c) {
+  let x1 = (-b+Math.sqrt(b*b-4*a*c)) / (2*a)
+  let x2 = (-b-Math.sqrt(b*b-4*a*c)) / (2*a)
+  return [x1,x2];
+}
+// Math.sqrt() returns square root of a number
