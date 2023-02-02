0735f1f130f17cd22925d9d16db067098135771a
Jakob Asher <pimpdogbark@gmail.com>
2023-02-02 17:53:16 -0500

    Create findNearestSquareNumber.js

---
+++ 8th kyu/findNearestSquareNumber.js
@@ -0,0 +1,6 @@
+function nearestSq(n){
+    return Math.pow(Math.round(Math.sqrt(n)),2)
+}
+// Math.pow() raised to power or too, can also try **
+// Math.round() rounds to nearest integer
+// Math.sqrt() square roots number
