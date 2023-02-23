1f37dc22f73d2805bc9d83118b400e2b5e040721
Jakob Asher <pimpdogbark@gmail.com>
2023-02-22 19:48:15 -0500

    Create beConciseITheTernaryOperator.js

---
+++ 8th kyu/beConciseITheTernaryOperator.js
@@ -0,0 +1,5 @@
+function describeAge(age) {
+  return "You're a(n) " + (age<=12?"kid":age<=17?"teenager":age<= 64?"adult":"elderly")
+}
+// Ternary if else, brackets matter
+// Less then 137 characters
