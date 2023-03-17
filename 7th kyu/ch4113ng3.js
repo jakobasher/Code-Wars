65e827e1b57c4fee6465b358282c4b17a3f42ef4
J <pimpdogbark@gmail.com>
2023-03-17 18:17:45 -0400

    Create ch4113ng3.js

---
+++ 7th kyu/ch4113ng3.js
@@ -0,0 +1,10 @@
+function nerdify(txt){
+  return txt
+    .replaceAll("a","4")
+    .replaceAll("A","4")
+    .replaceAll("e","3")
+    .replaceAll("E","3")
+    .replaceAll("l","1")
+}
+// .replaceAll() replace all pattern with
+// Can use REGEX, can use objects
