cbca1b7ec65f3f3fdb4162c8e329e5ce9d604190
J <pimpdogbark@gmail.com>
2023-03-18 15:49:57 -0400

    Create determineOffspringSexBasedOnGenesXXAndXYChromosomes.js

---
+++ 8th kyu/determineOffspringSexBasedOnGenesXXAndXYChromosomes.js
@@ -0,0 +1,6 @@
+function chromosomeCheck(sperm) {
+  return sperm == "XY" 
+    ? "Congratulations! You're going to have a son." 
+    : "Congratulations! You're going to have a daughter."  
+}
+// Can try template literals to shorten son/daughter portion
