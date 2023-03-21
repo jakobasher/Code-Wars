6319533c5c976bd10a933c1aef8651c4de1746ba
J <pimpdogbark@gmail.com>
2023-03-21 17:35:29 -0400

    Create areaOfAnAnnulus.js

---
+++ 7th kyu/areaOfAnAnnulus.js
@@ -0,0 +1,7 @@
+function annulusArea(a) {
+  return +(Math.PI*a*a / 4).toFixed(2)
+}
+// + to return as number
+// Math.PI = pi
+// .toFixed() return with precision
+// Question assumptions, R = 2r, (a/2)** a*a/4
