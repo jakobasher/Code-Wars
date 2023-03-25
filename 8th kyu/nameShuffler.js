ebcdfdd582a5fd30d50162a3c8404b58b3457a24
J <pimpdogbark@gmail.com>
2023-03-25 00:53:02 -0400

    Create nameShuffler.js

---
+++ 8th kyu/nameShuffler.js
@@ -0,0 +1,7 @@
+function nameShuffler(str){
+  return str.split(" ").reverse().join(" ")
+}
+// .split() returns array from string
+// .reverse() reverse index of array
+// .join() return string from array
+// Can try switching [0] and [1] other ways
