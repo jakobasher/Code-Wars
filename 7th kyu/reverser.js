45ef6f92286e095e76402c3e9466ac2b16664412
J <pimpdogbark@gmail.com>
2023-03-30 19:03:24 -0400

    Create reverser.js

---
+++ 7th kyu/reverser.js
@@ -0,0 +1,8 @@
+function reverse(n){
+  return +( ([]+n).split([]).reverse().join([]) )
+}
+// +var return as number
+// .split() returns array
+// .reverse() reverse array index
+// .join() return string
+//
