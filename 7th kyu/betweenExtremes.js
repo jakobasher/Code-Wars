1f045dcfafa9191484dea786a269df772e201b19
J <pimpdogbark@gmail.com>
2023-04-03 17:39:08 -0400

    Create betweenExtremes.js

---
+++ 7th kyu/betweenExtremes.js
@@ -0,0 +1,6 @@
+function betweenExtremes(numbers) {
+    return Math.max(...numbers) - Math.min(...numbers);
+}
+// Math.max() returns max value among
+// Math.min() returns min value among
+// ... Spready syntax array to iterible list
