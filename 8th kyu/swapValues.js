6e5139a43ce9269c3a1aa8b8d93672e53d8c0928
J <pimpdogbark@gmail.com>
2023-06-25 18:44:24 -0400

    Create swapValues.js

---
+++ 8th kyu/swapValues.js
@@ -0,0 +1,9 @@
+function swapValues(a) {
+//     var args = Array.prototype.slice.call(arguments);
+//     var temp = args[0];
+//     args[0] = args[1];
+//     args[1] = temp;
+  return a.reverse()
+}
+// .reverse() reverses index in array
+// can also do arguments[0].reverse
