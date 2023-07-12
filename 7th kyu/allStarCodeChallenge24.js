2b92cd0e4f10224f39851806f3397d56eb6f3f89
J <pimpdogbark@gmail.com>
2023-07-12 18:05:58 -0400

    Create allStarCodeChallenge24.js

---
+++ 7th kyu/allStarCodeChallenge24.js
@@ -0,0 +1,8 @@
+function hypotenuse(a, b){
+  return Math.pow(Math.pow(a,2)+Math.pow(b,2),0.5)    
+}
+
+function leg(c, a){
+  return Math.pow(Math.pow(c,2)-Math.pow(a,2),0.5)    
+}
+// Math.pow() base raised to exponent
