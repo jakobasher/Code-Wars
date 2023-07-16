86de55e9b8b98a4b344bdf0d56bc594fdb345053
J <pimpdogbark@gmail.com>
2023-07-16 00:46:31 -0400

    Create capitalizationAndMutability.js

---
+++ 8th kyu/capitalizationAndMutability.js
@@ -0,0 +1,6 @@
+function capitalizeWord(word) {
+//   word[0].toUpperCase();
+  return word[0].toUpperCase() + word.slice(1)
+}
+// .toUpperCase() returns as capital
+// .slice() return portion of string
