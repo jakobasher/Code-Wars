eff8f99ffdbcf98d41b909773841e87ccbe75794
J <pimpdogbark@gmail.com>
2023-07-30 15:32:46 -0400

    Create findMinAndMax.js

---
+++ 7th kyu/findMinAndMax.js
@@ -0,0 +1,5 @@
+function getMinMax(arr){
+  return [Math.min(...arr), Math.max(...arr)]
+}
+// Math.min() Math.max() return min/max of list (no array)
+// ... spread syntax use to iterate array to list
