a6b5bb3dd5782e9b9de6800ff760cc531a4767b0
J <pimpdogbark@gmail.com>
2023-08-11 18:08:11 -0400

    Create welcomeToTheCity.js

---
+++ 7th kyu/welcomeToTheCity.js
@@ -0,0 +1,5 @@
+function sayHello( name, city, state ) {
+    return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`
+}
+// `` ${} Template literal tools
+// .join() return string from array
