cf4516a7308f3de055ebe2717a065d4fc91d0c1a
J <pimpdogbark@gmail.com>
2023-08-18 17:02:02 -0400

    Create selectiveFearOfNumbers.js

---
+++ 7th kyu/selectiveFearOfNumbers.js
@@ -0,0 +1,10 @@
+var AmIAfraid = function(day, num){
+  if (day=="Monday" && num == 12) return true
+  if (day=="Tuesday" && num > 95) return true 
+  if (day=="Wednesday" && num == 34) return true
+  if (day=="Thursday" && num == 0) return true
+  if (day=="Friday" && num%2==0) return true
+  if (day=="Saturday" && num == 56) return true
+  if (day=="Sunday" && Math.abs(num)==666) return true
+  return false
+}
