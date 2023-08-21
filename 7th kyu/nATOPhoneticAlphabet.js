596500b4f9087d1ea45edd64fc384fb96465d536
J <pimpdogbark@gmail.com>
2023-08-21 16:06:22 -0400

    Create nATOPhoneticAlphabet.js

---
+++ 7th kyu/nATOPhoneticAlphabet.js
@@ -0,0 +1,21 @@
+var nato = (function() {
+  var letters =  {
+    "A": "Alpha",  "B": "Bravo",   "C": "Charlie",
+    "D": "Delta",  "E": "Echo",    "F": "Foxtrot",
+    "G": "Golf",   "H": "Hotel",   "I": "India",
+    "J": "Juliett","K": "Kilo",    "L": "Lima",
+    "M": "Mike",   "N": "November","O": "Oscar",
+    "P": "Papa",   "Q": "Quebec",  "R": "Romeo",
+    "S": "Sierra", "T": "Tango",   "U": "Uniform",
+    "V": "Victor", "W": "Whiskey", "X": "X-ray",
+    "Y": "Yankee", "Z": "Zulu"
+  }
+  
+  return function(word) {
+    return word.toUpperCase().split('').map(e => letters[e]).join(' ')
+  }
+})()
+// .toUpperCase() return string as all caps
+// .split() return as array
+// .map() Fn to each array element
+// .join() return as string
