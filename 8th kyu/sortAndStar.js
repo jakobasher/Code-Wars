3aaa9178b2dfd2666cb87e814a48f696e6f8c443
J <pimpdogbark@gmail.com>
2023-09-08 18:55:54 -0400

    Create sortAndStar.js

---
+++ 8th kyu/sortAndStar.js
@@ -0,0 +1,9 @@
+function twoSort(s) {
+  return s
+    .sort()[0]
+    .split("")
+    .join("***")
+}
+// .sort() sort by value then takes first 
+// .split() return as array
+// .join() return as string
