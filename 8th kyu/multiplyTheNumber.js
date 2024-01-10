2b8740b30fd0a23ead4aefa6e7d0a1d6236c4a51
J <pimpdogbark@gmail.com>
2024-01-09 20:44:39 -0500

    Create multiplyTheNumber.js

---
+++ 8th kyu/multiplyTheNumber.js
@@ -0,0 +1,6 @@
+function multiply(number){
+  return number * Math.pow(5, Math.abs(number).toString().length)
+}
+// Math.pow() base raised to exponent, can try **
+// Math.abs() returns absolute value
+// .toString() return as string
