8924b5816d5c639e78687d79616e138f08b2977b
J <pimpdogbark@gmail.com>
2025-03-06 17:39:03 -0500

    Create classConundrumBugFixing7.js

---
+++ 7th kyu/classConundrumBugFixing7.js
@@ -0,0 +1,15 @@
+class List {
+  constructor(type) {
+    this.type = type;
+    this.items = [];
+    this.count = 0;
+  }
+  add(item) {
+    if ( typeof item !== this.type )
+      return `This item is not of type: ${this.type}`;
+
+    this.items.push(item);
+    this.count++
+    return this;
+  }
+}
