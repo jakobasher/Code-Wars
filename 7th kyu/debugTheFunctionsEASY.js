a6a45687e787a1ef4916248f9ed01885831362e0
J <pimpdogbark@gmail.com>
2025-03-14 17:53:56 -0400

    Create debugTheFunctionsEASY.js

---
+++ 7th kyu/debugTheFunctionsEASY.js
@@ -0,0 +1,9 @@
+function multi(arr) {
+  return arr.reduce((a,c)=>a*c,1)
+}
+function add(arr) {
+  return arr.reduce((a,c)=>a+c,0)
+}
+function reverse(str) {
+  return str.split("").reverse().join("")
+}
